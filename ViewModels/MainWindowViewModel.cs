07e1c633fb08a615c8263dec224674e9954d2c45
Luka Lovre <lukalovre@protonmail.com>
2024-07-29 08:59:21 +0200

    removed not used classes

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -5,25 +5,5 @@ namespace AvaloniaApplication1.ViewModels;
 
 public class MainWindowViewModel(IDatasource datasource) : ViewModelBase
 {
-    public MoviesViewModel MoviesViewModel { get; } = new MoviesViewModel(datasource, new MovieExternal());
-    public StandupViewModel StandupViewModel { get; } = new StandupViewModel(datasource, new StandupExternal());
-    public TheatreViewModel TheatreViewModel { get; } = new TheatreViewModel(datasource);
-    public MusicViewModel MusicViewModel { get; } = new MusicViewModel(datasource, new MusicExternal());
-    public WorkViewModel WorkViewModel { get; } = new WorkViewModel(datasource);
-    public BooksViewModel BooksViewModel { get; } = new BooksViewModel(datasource, new BookExtetrnal());
-    public ComicsViewModel ComicsViewModel { get; } = new ComicsViewModel(datasource, new ComicExtetrnal());
-    public GamesViewModel GamesViewModel { get; } = new GamesViewModel(datasource, new GameExtetrnal());
-    public TVShowsViewModel TVShowsViewModel { get; } = new TVShowsViewModel(datasource, new TVShowExternal());
-    public ClipsViewModel ClipsViewModel { get; } = new ClipsViewModel(datasource, new ClipsExternal());
-    public SongsViewModel SongsViewModel { get; } = new SongsViewModel(datasource, new SongExternal());
-    public ZooViewModel ZooViewModel { get; } = new ZooViewModel(datasource);
-    public MagazinesViewModel MagazinesViewModel { get; } = new MagazinesViewModel(datasource);
-    public BoardgamesViewModel BoardgamesViewModel { get; } = new BoardgamesViewModel(datasource);
-    public DnDViewModel DnDViewModel { get; } = new DnDViewModel(datasource);
-    public ClassicalViewModel ClassicalViewModel { get; } = new ClassicalViewModel(datasource);
-    public PaintingsViewModel PaintingsViewModel { get; } = new PaintingsViewModel(datasource);
-    public PinballViewModel PinballViewModel { get; } = new PinballViewModel(datasource);
-    public PeopleViewModel PeopleViewModel { get; } = new PeopleViewModel(datasource);
-    public ConcertsViewModel ConcertsViewModel { get; } = new ConcertsViewModel(datasource);
-    public LocationsViewModel LocationsViewModel { get; } = new LocationsViewModel(datasource);
+    public GamesViewModel GamesViewModel { get; } = new GamesViewModel(datasource, null);
 }
--- obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/AvaloniaApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf4d64eff0894f573118881d70a6a232a48208e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76bf1a20642d79242dfba6ceced194de66d8dab7")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
